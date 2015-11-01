71f4a523b470e720c4727b7f9ee76a527f1b2843
Henning Leutz <leutz@pcsg.de>
2015-11-01 10:43:02 +0100

    Smarty Events

---
+++ src/QUI/FontAwesome/EventHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * This file contains QUI\FontAwesome\EventHandler
+ */
+namespace QUI\FontAwesome;
+
+use QUI;
+
+/**
+ * Class EventHandler
+ * @package QUI\FontAwesome
+ */
+class EventHandler
+{
+
+    /**
+     * Event : on smarty init
+     * @param \Smarty $Smarty - \Smarty
+     */
+    static function onSmartyInit($Smarty)
+    {
+        // {fontawesome}
+        if (!isset($Smarty->registered_plugins['function']) ||
+            !isset($Smarty->registered_plugins['function']['fontawesome'])
+        ) {
+            $Smarty->registerPlugin("function", "fontawesome", "\QUI\FontAwesome\EventHandler::fontawesome");
+        }
+    }
+
+    /**
+     * Smarty brickarea function {brickarea}
+     *
+     * @param Array $params - function parameter
+     * @param \Smarty $smarty - \Smarty
+     * @return Array|String
+     */
+    static function fontawesome($params, $smarty)
+    {
+        return '<link href="'. URL_OPT_DIR .'bin/unsemantic/assets/stylesheets/unsemantic-grid-responsive.css"
+          rel="stylesheet"
+          type="text/css"
+               />';
+    }
+}
